58ea83ad9790d7241eba7ec97b1bc7174b7edbee
Ilana Davis <ilana@ilanadavis.com>
2017-12-14 19:56:24 +0000

    js sample work

---
+++ JavaScript_course/js_sample.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1 id="text"></h1>
+
+<script>
+
+var text = document.getElementByID("text");
+text.innerHTML = "Hello World!";
+
+
+
+</script>
+
+
+</body>
+</html>
