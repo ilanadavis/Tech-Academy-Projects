b9c91786b1aa980adff2036154fca6e109c1e70b
Ilana Davis <ilana@ilanadavis.com>
2017-12-18 04:05:59 +0000

    add get veggies

--- Pizza_project_JScourse/pizza_project.js
+++ Pizza_project_JScourse/pizza_project.js
@@ -26,13 +26,14 @@ function getReceipt() {
     console.log(selectedSize+" = $"+sizeTotal+".00");
     console.log("size text1: "+text1);
     console.log("subtotal: $"+runningTotal+".00");
-    getProtein(runningTotal,text1); // All three of these variables will be passed on to each function
+    getProtein(runningTotal,text1);
+    getVeggies(runningTotal,text1);// All three of these variables will be passed on to each function
 }
 	
 function getProtein(runningTotal,text1) {
 	var proteinTotal = 0;
 	var selectedProtein = [];
-	var proteinArray = document.getElementsByClassName("protein");
+	var proteinArray = document.getElementsByClassName("Protein");
 	for (var j = 0; j < proteinArray.length; j++) {
 		if (proteinArray[j].checked) {
 			selectedProtein.push(proteinArray[j].value);
@@ -46,10 +47,36 @@ function getProtein(runningTotal,text1) {
 	} else {
 		proteinTotal = 0;
 	}
-	runningTotal = (runningTotal + proteinTotal);
+	runningTotal += proteinTotal;
 	console.log("total selected protein items: "+proteinCount);
 	console.log(proteinCount+" protein - 1 free protein = "+"$"+proteinTotal+".00");
 	console.log("protein text1: "+text1);
+	console.log("Purchase Total: "+"$"+runningTotal+".00");
+	document.getElementById("showText").innerHTML=text1;
+	document.getElementById("totalPrice").innerHTML = "</h3>Total: <strong>$"+runningTotal+".00"+"</strong></h3>";
+}	
+
+function getVeggies(runningTotal,text1) {
+	var veggiesTotal = 0;
+	var selectedVeggies = [];
+	var veggiesArray = document.getElementsByClassName("Veggies");
+	for (var k = 0; k < veggiesArray.length; k++) {
+		if (veggiesArray[k].checked) {
+			selectedVeggies.push(veggiesArray[k].value);
+			console.log("selected veggies item: ("+veggiesArray[k].value+")");
+			text1 = text1+veggiesArray[k].value+"<br>";
+		}
+	}
+	var veggiesCount = selectedVeggies.length;
+	if (veggiesCount > 1) {
+		veggiesTotal = (veggiesCount - 1);
+	} else {
+		veggiesTotal = 0;
+	}
+	runningTotal += veggiesTotal;
+	console.log("total selected veggie items: "+veggiesCount);
+	console.log(veggiesCount+" veggie - 1 free veggie = "+"$"+veggiesTotal+".00");
+	console.log("veggie text1: "+text1);
 	console.log("Purchase Total: "+"$"+runningTotal+".00");
 	document.getElementById("showText").innerHTML=text1;
 	document.getElementById("totalPrice").innerHTML = "</h3>Total: <strong>$"+runningTotal+".00"+"</strong></h3>";
