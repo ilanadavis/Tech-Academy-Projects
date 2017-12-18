f7027cbb85a17325fa131aeb94db238798a765af
Ilana Davis <ilana@ilanadavis.com>
2017-12-18 06:59:27 +0000

    minor changes to sizing & wording

--- Pizza_project_JScourse/pizza_project.js
+++ Pizza_project_JScourse/pizza_project.js
@@ -24,44 +24,43 @@ function getReceipt() {
     console.log(selectedSize+" = $"+sizeTotal+".00");
     console.log("subtotal: $"+runningTotal+".00");
     
-    // TODO Add additional charges of variables
-    var proteinBox = getProtein(); //names of proteins selected and dollar amounts for proteins
-	var proteinLineItem = proteinBox[0]; //list of proteins selected
-	var proteinTotal = proteinBox[1]; //dollar amounts for proteins
+    var proteinChoice = getProtein(); //names of proteins selected and dollar amounts for proteins
+	var proteinLineItem = proteinChoice[0]; //list of proteins selected
+	var proteinTotal = proteinChoice[1]; //dollar amounts for proteins
 
-    var veggiesBox = getVeggies(); //names of veggies selected and dollar amounts for veggies
-	var veggiesLineItem = veggiesBox[0]; //list of veggies selected
-	var veggiesTotal = veggiesBox[1]; //dollar amounts for veggies
+    var veggiesChoice = getVeggies(); //names of veggies selected and dollar amounts for veggies
+	var veggiesLineItem = veggiesChoice[0]; //list of veggies selected
+	var veggiesTotal = veggiesChoice[1]; //dollar amounts for veggies
 
-    var cheeseBox = getCheese(); //cheese selected and dollar amounts for cheese
-    var selectedCheese = cheeseBox[0]; //selected cheese
-    var cheeseTotal = cheeseBox[1]; //dollar amount for cheese
+    var cheeseChoice = getCheese(); //cheese selected and dollar amounts for cheese
+    var selectedCheese = cheeseChoice[0]; //selected cheese
+    var cheeseTotal = cheeseChoice[1]; //dollar amount for cheese
 
-    var crustBox = getCrust(); //crust selected and dollar amounts for crust
-    var selectedCrust = crustBox[0]; //selected crust
-    var crustTotal = crustBox[1]; //dollar amount for crust
+    var crustChoice = getCrust(); //crust selected and dollar amounts for crust
+    var selectedCrust = crustChoice[0]; //selected crust
+    var crustTotal = crustChoice[1]; //dollar amount for crust
 
-    var sauceBox = getSauce(); //sauce selected and dollar amounts for sauce
-    var selectedSauce = sauceBox[0]; //selected sauce
-    var sauceTotal = sauceBox[1]; //dollar amount for sauce
+    var sauceChoice = getSauce(); //sauce selected and dollar amounts for sauce
+    var selectedSauce = sauceChoice[0]; //selected sauce
+    var sauceTotal = sauceChoice[1]; //dollar amount for sauce
 
     runningTotal = sizeTotal + proteinTotal + veggiesTotal + cheeseTotal + crustTotal + sauceTotal;
     console.log("Purchase Total: "+"$"+runningTotal+".00");
 
-	var receiptTable = "<table>"
+	var receiptTable = "<table>";
 	receiptTable+= writeReceipt(selectedSize, "$"+sizeTotal+".00");
-	receiptTable+= writeReceipt(proteinLineItem, "+$"+proteinTotal+".00");
-	receiptTable+= writeReceipt(veggiesLineItem, "+$"+veggiesTotal+".00");
+	receiptTable+= writeReceipt(proteinLineItem || "No Protein", "+$"+proteinTotal+".00");
+	receiptTable+= writeReceipt(veggiesLineItem || "No Veggies", "+$"+veggiesTotal+".00");
 	receiptTable+= writeReceipt(selectedCheese, "+$"+cheeseTotal+".00");
 	receiptTable+= writeReceipt(selectedCrust, "+$"+crustTotal+".00");
 	receiptTable+= writeReceipt(selectedSauce, "+$"+sauceTotal+".00");	
-	receiptTable+= "</table>"
-	document.getElementById("showText").innerHTML="<h3>You Ordered:</h3>"+"<br>"+receiptTable;
+	receiptTable+= "</table>";
+	document.getElementById("showText").innerHTML="<h3>You Ordered:</h3>"+receiptTable;
 
-	document.getElementById("totalPrice").innerHTML = "</h3>Total: <strong>$"+runningTotal+".00"+"</strong></h3>";
+	document.getElementById("totalPrice").innerHTML = "<h3>Total: <strong>$"+runningTotal+".00"+"</strong></h3>";
 }
 function writeReceipt(lineItem, linePrice) {
-	return "<tr><td>"+lineItem+"</td><td>"+linePrice+"</td></tr>"
+	return "<tr><td>"+lineItem+"</td><td>"+linePrice+"</td></tr>";
 }
 function getProtein() {
 	var proteinTotal = 0;
@@ -123,7 +122,7 @@ function getCrust() {
 	if (selectedCrust == "Cheese Stuffed Crust") {
 		crustTotal = 3;
 	} else {
-	crustTotal = 0;
+		crustTotal = 0;
 	}
 	console.log("total selected crust items: "+crustTotal);
 	return [selectedCrust, crustTotal];  // crust selection and crust subtotal
@@ -135,7 +134,7 @@ function getSauce() {
 	if (selectedSauce == "") {
 		sauceTotal = 0;
 	} else {
-	sauceTotal = 0;
+		sauceTotal = 0;
 	}
 	console.log("total selected sauce items: "+sauceTotal);
 	return [selectedSauce, sauceTotal];  // sauce selection and sauce subtotal
