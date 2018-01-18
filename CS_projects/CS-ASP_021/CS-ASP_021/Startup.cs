44916c4defd5a09fc3870d0100eb09ed30108d21
Ilana Davis <ilana@ilanadavis.com>
2018-01-18 14:40:30 -0800

    practice with arrays

---
+++ CS_projects/CS-ASP_021/CS-ASP_021/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CS_ASP_021.Startup))]
+namespace CS_ASP_021
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
