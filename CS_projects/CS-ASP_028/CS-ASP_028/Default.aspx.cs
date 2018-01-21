11b1097b84044e178aca0da687b01ba109d95ae6
Ilana Davis <ilana@ilanadavis.com>
2018-01-21 09:39:45 -0800

    practice with input parameters

--- CS_projects/CS-ASP_028/CS-ASP_028/Default.aspx.cs
+++ CS_projects/CS-ASP_028/CS-ASP_028/Default.aspx.cs
@@ -17,8 +17,12 @@ namespace CS_ASP_028
         protected void cupsRadio_CheckedChanged(object sender, EventArgs e)
         {
             //using the method from below in each section rather than typing out code for each one
-            calculateCups();
-          
+            //calculateCups();
+
+            calculateCups(1.0, "cups");
+
+
+
             /*  if (quantityTextBox.Text.Trim().Length == 0)
                 return;
 
@@ -33,8 +37,9 @@ namespace CS_ASP_028
         protected void fromPintsRadio_CheckedChanged(object sender, EventArgs e)
         {
             //using the method from below in each section rather than typing out code for each one
-            calculateCups();
+            //calculateCups();
 
+            calculateCups(2.0, "pints");
 
             /* if (quantityTextBox.Text.Trim().Length == 0)
                  return;
@@ -52,7 +57,8 @@ namespace CS_ASP_028
         {
 
             //using the method from below in each section rather than typing out code for each one
-            calculateCups();
+            // calculateCups();
+            calculateCups(4.0, "quarts");
 
 
             /*  if (quantityTextBox.Text.Trim().Length == 0)
@@ -71,7 +77,9 @@ namespace CS_ASP_028
         {
 
             //using the method from below in each section rather than typing out code for each one
-            calculateCups();
+            //calculateCups();
+
+            calculateCups(16.0, "gallons");
 
 
             /* if (quantityTextBox.Text.Trim().Length == 0)
@@ -89,12 +97,12 @@ namespace CS_ASP_028
         protected void quantityTextBox_TextChanged(object sender, EventArgs e)
         {
             //using the method from below in each section rather than typing out code for each one
-            calculateCups();
+          //  calculateCups();
         }
 
 
         //creates a method that can be called above rather than having to type each if statement for each radio button (DRY dont repeat yourself)
-
+        /*
         private void calculateCups()
         {
             if (quantityTextBox.Text.Trim().Length == 0)
@@ -111,6 +119,24 @@ namespace CS_ASP_028
             else if (fromGallonsRadio.Checked) cups = quantity * 16;
 
             resultLabel.Text = "The number of cups: " + cups.ToString();
+            */
+
+
+            //working with input parameters to eliminate the code directly above
+        private void calculateCups(double measureToCupRatio, string measureName)
+        {
+            if (quantityTextBox.Text.Trim().Length == 0)
+                return;
+
+            double quantity = 0.0;
+            if (!Double.TryParse(quantityTextBox.Text, out quantity))
+                return;
+
+            //define an input parameter called measureToCupRatio
+            double cups = quantity * measureToCupRatio;
+           
+
+            resultLabel.Text = String.Format("{0:N2} {1} is equal to {2:N2} cups.", quantity, measureName, cups);
         }
     }
 }
