2960e68950566ee86848b2ce5d72f735dda27830
Ilana Davis <ilana@ilanadavis.com>
2018-01-24 10:58:28 -0800

    add code for matching reels to determine win or loss

--- CS_projects/MegaChallengeCasino/MegaChallengeCasino/Default1.aspx.cs
+++ CS_projects/MegaChallengeCasino/MegaChallengeCasino/Default1.aspx.cs
@@ -9,20 +9,20 @@ namespace MegaChallengeCasino
 {
     public partial class Default1 : System.Web.UI.Page
     {
+        //image of reels are random
+        Random random = new Random();
+
+        //player begins with 100
+        double playersMoney = 100;
+        double betAmount = 0;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             reelOne.ImageUrl = reelImageDisplay();
             reelTwo.ImageUrl = reelImageDisplay();
             reelThree.ImageUrl = reelImageDisplay();
         }
-        //image of reels are random
-        Random random = new Random();
        
-        //player begins with 100
-        int playersMoney = 100;
-        int betAmount = 0;
-
-
         //images on the reels
         public string reelImageDisplay()
             {
@@ -38,6 +38,10 @@ namespace MegaChallengeCasino
 
         protected void leverButton_Click(object sender, EventArgs e)
         {
+            reelOne.ImageUrl = reelImageDisplay();
+            reelTwo.ImageUrl = reelImageDisplay();
+            reelThree.ImageUrl = reelImageDisplay();
+            winORlose(betAmount);
         }
 
         private void calculatePlayersMoney(double playersMoney, double playersBet)
@@ -48,24 +52,68 @@ namespace MegaChallengeCasino
             if (!Double.TryParse(betTextBox.Text, out betAmount))
                 return;
 
+            //reels are randomly displayed at page open and when clicked
+            //click button to play and change reels
+            //playersMoney keeps track of balance as money is added and subtracted
+            //results are displayed that shows if player won or lost and displays running balance for playersMoney
             
         }
-        //following code is to be deleted
-        private void calculateCups(double measureToCupRatio, string measureName)
+
+        //if the player is a winner then add betAmount to playersMoney. Else subtract betAmount from playersMoney
+        public void winORlose(double betAmount)
         {
-            if (quantityTextBox.Text.Trim().Length == 0)
-                return;
+            //reels with a bar lose
+            if (anyReelMatch("images/Bar.png"))
+            {
+                playersMoney = playersMoney - betAmount;
+            }
+            //reels with 1 cherry wins 2x bet
+            //reels with 2 cherries wins 3x bet
+            //reels with 3 cherries wins 4x bet
+            else if (anyReelMatch("images/Cherry.png"))
+            {
+                playersMoney = playersMoney + ((countReelMatch("images/Cherry.png") + 1) * betAmount);
+            }
+            //reels with 3 7s wins 100x bet
+            else if (allReelMatch("images/Seven.png"))
+            {
+                playersMoney = playersMoney + (betAmount * 100);
+            }
+            else
+            {
+                playersMoney = playersMoney - betAmount;
+            }
+        }
 
-            double quantity = 0.0;
-            if (!Double.TryParse(quantityTextBox.Text, out quantity))
-                return;
+        public bool anyReelMatch(string reelImageToMatch)
+        {
+            return (reelOne.ImageUrl == reelImageToMatch || reelTwo.ImageUrl == reelImageToMatch || reelThree.ImageUrl == reelImageToMatch);
+        }
+        public bool allReelMatch(string reelImageToMatch)
+        {
+            return (reelOne.ImageUrl == reelImageToMatch && reelTwo.ImageUrl == reelImageToMatch && reelThree.ImageUrl == reelImageToMatch);
+        }
+        //check each reel indiv and if it matches, increment counter and return counter at the end
+        public int countReelMatch(string reelImageToMatch)
+        {
+            int counter = 0;
+            if (reelOne.ImageUrl == reelImageToMatch)
+            {
+                counter += 1;
+            }
+            if (reelTwo.ImageUrl == reelImageToMatch)
+            {
+                counter += 1;
+            }
+            if (reelThree.ImageUrl == reelImageToMatch)
+            {
+                counter += 1;
+            }
+            return counter;
+        }
 
-            //define an input parameter called measureToCupRatio
-            double cups = quantity * measureToCupRatio;
 
-
-
-        private void displayResults(int betTextBox, int playersMoney)
+        private void displayResults(double betTextBox, double playersMoney)
         {
             if (reelImageDisplay() == "images/Bar.png")
             {
