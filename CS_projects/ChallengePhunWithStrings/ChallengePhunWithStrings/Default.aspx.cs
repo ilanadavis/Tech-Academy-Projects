54085c969d18eb021c6997f28f354121140e35d0
Ilana Davis <ilana@ilanadavis.com>
2018-01-26 11:41:03 -0800

    add padding equation for problem 3

--- CS_projects/ChallengePhunWithStrings/ChallengePhunWithStrings/Default.aspx.cs
+++ CS_projects/ChallengePhunWithStrings/ChallengePhunWithStrings/Default.aspx.cs
@@ -9,6 +9,7 @@ namespace ChallengePhunWithStrings
 {
     public partial class Default : System.Web.UI.Page
     {
+
         protected void Page_Load(object sender, EventArgs e)
         {
             // 1.  Reverse your name
@@ -24,36 +25,32 @@ namespace ChallengePhunWithStrings
             string names = "Luke,Leia,Han,Chewbacca";
             // When you're finished it should look like this:
             // Chewbacca,Han,Leia,Luke
+            /*
+            string[] namesArray = names.Split(',');
+            Array.Reverse(namesArray);
 
-            //string[] namesArray = names.Split(',');
-            //Array.Reverse(namesArray);
-
-            // resultLabel.Text = String.Join(",", namesArray);
-
+            resultLabel.Text = String.Join(",", namesArray);
+            */
 
             // 3. Use the sequence to create ascii art:
-            // *****luke*****
-            // *****leia*****
-            // *****han******
-            // **Chewbacca***
+            // *****luke*****  5,4,5
+            // *****leia*****  5,4,5
+            // *****han******  5,3,6
+            // **Chewbacca***  2,9,3
 
-
-            string[] namesArray = names.Split(',');
-          
-           // for (int i = 0; i <= names.Length; i--)
+            
+            string[] split = names.Split(new Char[] { ' ', ',', '.', ':', '\t' });
+            foreach (string s in split)
             {
-                resultLabel.Text += names;
+
+                if (s.Trim() != "")
+                    resultLabel.Text += s.ToString().PadRight(((14 - split.Length) / 2) + split.Length,'*').PadLeft(14, '*') + "<br />";
             }
+            //not padding Chewbacca correctly. Need to revisit. 
 
+        // 4. Solve this puzzle:
 
-           // resultLabel.Text = namesArray.PadLeft(30, '#');
-
-
-
-
-            // 4. Solve this puzzle:
-
-            string puzzle = "NOW IS ZHEremove-me ZIME FOR ALL GOOD MEN ZO COME ZO ZHE AID OF ZHEIR COUNZRY.";
+        string puzzle = "NOW IS ZHEremove-me ZIME FOR ALL GOOD MEN ZO COME ZO ZHE AID OF ZHEIR COUNZRY.";
 
             // Once you fix it with string helper methods, it should read:
             // Now is the time for all good men to come to the aid of their country.
