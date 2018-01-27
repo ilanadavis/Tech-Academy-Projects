d225de2f682e1d4608f9aedd9c3304210d3b678f
Ilana Davis <ilana@ilanadavis.com>
2018-01-26 16:25:52 -0800

    begining build of hero monster challenge. Create class for Character with properties and begin assignment.

---
+++ CS_projects/ChallengeHeroMonsterClassesPart1/ChallengeHeroMonsterClassesPart1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChallengeHeroMonsterClassesPart1.Startup))]
+namespace ChallengeHeroMonsterClassesPart1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
