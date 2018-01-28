006a6cc16432352a217ce99d61946f3d426490e5
Ilana Davis <ilana@ilanadavis.com>
2018-01-28 11:53:05 -0800

    fix attack and defend methods. Begin work on battle method

--- CS_projects/ChallengeHeroMonsterClassesPart1/ChallengeHeroMonsterClassesPart1/Default1.aspx.cs
+++ CS_projects/ChallengeHeroMonsterClassesPart1/ChallengeHeroMonsterClassesPart1/Default1.aspx.cs
@@ -11,7 +11,46 @@ namespace ChallengeHeroMonsterClassesPart1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Character hero = new Character();
+            hero.Name = "hero";
+            hero.Health = 100;
+            hero.DamageMaximum = 5;
+            hero.AttackBonus = 10;
 
+            Character monster = new Character();
+            monster.Name = "monster";
+            monster.Health = 100;
+            monster.DamageMaximum = 20;
+            monster.AttackBonus = 5;
+            
         }
     }
+
+    class Character
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int DamageMaximum { get; set; }
+        public int AttackBonus { get; set; }
+
+        public int Attack()
+        {
+            //Attack() return an int -randomly determine the amount of damage this Character object inflicted.
+            Random random = new Random();
+            int damage = random.Next(1, DamageMaximum);
+
+        }
+        public int Defend(int damage)
+        {
+            //Defend(int damage) - deducts the damage from this Character's health
+            remainingDefenderHealth = defenderHealth - damage;
+
+        }
+
+    }
 }
+/*
+You will perform two sides of the battle.  The first half of the battle will involve the hero attacking the monster and the monster defending itself.  
+The second half of the battle will involve the monster attacking and the hero defending itself.  Just one round -- this is not a fight to the death.
+
+Create a helper method in the Deafult class to display the stats of each character in a Label server control.
