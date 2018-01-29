19a21046522e03784cbd30306a7f3d762534766b
Ilana Davis <ilana@ilanadavis.com>
2018-01-28 17:38:44 -0800

    challenge hero monster classes part 2. Add new class: Dice and property: Sides. Add parameters and change resultLabel to accomodate parameters.

--- CS_projects/ChallengeHeroMonsterClassesPart1/ChallengeHeroMonsterClassesPart1/Default1.aspx.cs
+++ CS_projects/ChallengeHeroMonsterClassesPart1/ChallengeHeroMonsterClassesPart1/Default1.aspx.cs
@@ -12,53 +12,107 @@ namespace ChallengeHeroMonsterClassesPart1
         public void characterStats(Character character)
         {
             resultLabel.Text += string.Format("<p>Name: {0}, Health: {1}, DamageMaximum: {2}, AttackBonus: {3}", 
-                character.Name, character.Health, character.DamageMaximum, character.AttackBonus);
+                character.Name, character.Health, character.DamageMaximum, character.AttackBonus.ToString());
         }
-    protected void Page_Load(object sender, EventArgs e)
+        protected void Page_Load(object sender, EventArgs e)
         {
             Character hero = new Character();
             hero.Name = "Hero";
             hero.Health = 30;
             hero.DamageMaximum = 15;
-            hero.AttackBonus = 10;
+            hero.AttackBonus = true;
 
             Character monster = new Character();
             monster.Name = "Monster";
             monster.Health = 30;
             monster.DamageMaximum = 20;
-            monster.AttackBonus = 5;
+            monster.AttackBonus = false;
 
-            //hero attacks monster and monster defends
-            int heroDamage = hero.Attack();
-            monster.Defend(heroDamage);
+            Dice heroRoll = new Dice();
+            Dice monsterRoll = new Dice();
 
-            //monster attacks hero and hero defends
-            int monsterDamage = monster.Attack();
-            hero.Defend(monsterDamage);
+            //battle loop fight to death. Only break out when one or both is <= 0
+            while (hero.Health > 0 && monster.Health > 0)
+            {
+                if (hero.AttackBonus == true)
+                {
+                    monster.Defend(hero.Attack(heroRoll));
+                }
+                if (monster.AttackBonus == true)
+                {
+                    hero.Defend(monster.Attack(monsterRoll));
+                }
 
-            characterStats(hero);
-            characterStats(monster);
+                //hero attacks monster and monster defends
+                int heroDamage = hero.Attack(heroRoll);
+                monster.Defend(heroDamage);
+
+                //monster attacks hero and hero defends
+                int monsterDamage = monster.Attack(heroRoll);
+                hero.Defend(monsterDamage);
+
+                characterStats(hero);
+                characterStats(monster);
+                displayResult(hero.Health, monster.Health);
+            }
         }
-    }
+   
+
+        //print results. Hero defeated Monster or visa versa or both died. Using character stats and put it inside each iteration
+        private void displayResult(int heroHealth, int monsterHealth)
+        {
+            if (monsterHealth <= 0 && heroHealth <= 0)
+            {
+                resultLabel.Text  += "<h3>They both lose!!</h3>";
+            }
+            else if (monsterHealth <= 0)
+            {
+                resultLabel.Text += "<h3>Hero wins!</h3>";
+            }
+            else if (heroHealth <= 0)
+            {
+                resultLabel.Text += "<h3>Monster wins!</h3>";
+            }
+            else
+            {
+                    
+            }
+                    
+        }
+}
 
     public class Character
     {
         public string Name { get; set; }
         public int Health { get; set; }
         public int DamageMaximum { get; set; }
-        public int AttackBonus { get; set; }
+        public bool AttackBonus { get; set; }
 
-        public int Attack()
+        public int Attack(Dice characterRoll)
         {
-            Random random = new Random();
-            int characterDamage = random.Next(1, DamageMaximum);
-            return characterDamage;
+            characterRoll.Sides = DamageMaximum;
+            return characterRoll.Roll();
         }
 
         public int Defend(int characterDamage)
         {
             Health = Health - characterDamage;
             return Health;
+        }
+    }
+
+    public class Dice
+    {
+        public int Sides { get; set; }
+
+        Random random = new Random();
+        //max damage = to number of sides on the dice
+
+        //Roll() method will use the Sides property as the max value used by random.next method()
+       public int Roll()
+        {
+            int characterRoll = random.Next(1, Sides);
+            return characterRoll;
         }
     }
 }
