c7d83a468eb364ede803ef7a74bc34e4092f4162
Ilana Davis <ilana@ilanadavis.com>
2018-01-29 08:54:58 -0800

    clean up of extra or undeeded lines

--- CS_projects/ChallengeHeroMonsterClassesPart1/ChallengeHeroMonsterClassesPart1/Default1.aspx.cs
+++ CS_projects/ChallengeHeroMonsterClassesPart1/ChallengeHeroMonsterClassesPart1/Default1.aspx.cs
@@ -34,50 +34,35 @@ namespace ChallengeHeroMonsterClassesPart1
             //battle loop fight to death. Only break out when one or both is <= 0
             while (hero.Health > 0 && monster.Health > 0)
             {
-                if (hero.AttackBonus == true)
-                {
-                    monster.Defend(hero.Attack(heroRoll));
-                }
-                if (monster.AttackBonus == true)
-                {
-                    hero.Defend(monster.Attack(monsterRoll));
-                }
+                if (hero.AttackBonus == true) monster.Defend(hero.Attack(heroRoll));
+                if (monster.AttackBonus == true) hero.Defend(monster.Attack(monsterRoll));
 
                 //hero attacks monster and monster defends
                 int heroDamage = hero.Attack(heroRoll);
                 monster.Defend(heroDamage);
 
                 //monster attacks hero and hero defends
-                int monsterDamage = monster.Attack(heroRoll);
+                int monsterDamage = monster.Attack(monsterRoll);
                 hero.Defend(monsterDamage);
 
+                //display of stats and results
                 characterStats(hero);
                 characterStats(monster);
                 displayResult(hero.Health, monster.Health);
             }
         }
    
-
         //print results. Hero defeated Monster or visa versa or both died. Using character stats and put it inside each iteration
         private void displayResult(int heroHealth, int monsterHealth)
         {
             if (monsterHealth <= 0 && heroHealth <= 0)
-            {
-                resultLabel.Text  += "<h3>They both lose!!</h3>";
-            }
+                resultLabel.Text += "<h3>They both lose!!</h3>";
             else if (monsterHealth <= 0)
-            {
                 resultLabel.Text += "<h3>Hero wins!</h3>";
-            }
             else if (heroHealth <= 0)
-            {
                 resultLabel.Text += "<h3>Monster wins!</h3>";
-            }
-            else
-            {
-                    
-            }
-                    
+            else { }
+                   
         }
 }
 
@@ -90,29 +75,25 @@ namespace ChallengeHeroMonsterClassesPart1
 
         public int Attack(Dice characterRoll)
         {
-            characterRoll.Sides = DamageMaximum;
-            return characterRoll.Roll();
+            characterRoll.Sides = DamageMaximum; return characterRoll.Roll();
         }
 
         public int Defend(int characterDamage)
         {
-            Health = Health - characterDamage;
-            return Health;
+            Health = Health - characterDamage; return Health;
         }
     }
 
     public class Dice
     {
         public int Sides { get; set; }
-
-        Random random = new Random();
         //max damage = to number of sides on the dice
+        Random random = new Random();
 
         //Roll() method will use the Sides property as the max value used by random.next method()
        public int Roll()
         {
-            int characterRoll = random.Next(1, Sides);
-            return characterRoll;
+            int characterRoll = random.Next(1, Sides); return characterRoll;
         }
     }
 }
