1c188a893968473e96226568edb3207921876746
Ilana Davis <ilana@ilanadavis.com>
2018-01-30 11:51:52 -0800

    Set up of system for challenge. Write pseudo code

---
+++ CS_projects/ChallengeSimpleDarts/ChallengeSimpleDarts/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace ChallengeSimpleDarts.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ CS_projects/ChallengeSimpleDarts/ChallengeSimpleDarts/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChallengeSimpleDarts.Startup))]
+namespace ChallengeSimpleDarts
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
