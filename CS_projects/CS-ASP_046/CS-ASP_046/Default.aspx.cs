00d8374e0c3d47314d522750a532eda83501c548
Ilana Davis <ilana@ilanadavis.com>
2018-02-01 11:31:11 -0800

    practice with list collections

---
+++ CS_projects/CS-ASP_046/CS-ASP_046/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_046
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string result = "";
+
+            Car car1 = new Car("BMW", "528i", 2010, "Black");
+            Car car2 = new Car("BMW", "745li", 2005, "Black");
+            Car car3 = new Car("Ford", "EScape", 2008, "White");
+
+            List<Car> cars = new List<Car>();
+
+            cars.Add(car1);
+            cars.Add(car2);
+            cars.Add(car3);
+
+            //Find all cars where color of car is white and when return, return as list of car
+            //List<Car> whiteCars = cars.FindAll(p => p.Color == "White");
+
+            //able to chancge all cars color to Silver
+            //cars.ForEach(p => p.Color = "Silver");
+
+
+            if (cars.Exists(p => p.Year == 2008))
+                result += "We do have a 2008 model year in stock!";
+
+            for (int i = 0; i < cars.Count; i++)
+            {
+                result += cars.ElementAt(i).FormatDetailsForDisplay();
+            }
+
+
+            resultLabel.Text = result;
+        }
+    }
+}
