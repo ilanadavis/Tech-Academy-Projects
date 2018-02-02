eaefa617dc8c7444bb5c2a7a240bc07b90c01b15
Ilana Davis <ilana@ilanadavis.com>
2018-02-01 17:29:17 -0800

    Add initializers, constructors and foreach loops with result

---
+++ CS_projects/ChallengeStudentCourses/ChallengeStudentCourses/Course.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChallengeStudentCourses
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string Name { get; set; }
+        public List<Student> Students { get; set; }
+
+        public Course()
+        {
+            this.CourseId = 0;
+            this.Name = "Name";
+            this.Students = Students;
+        }
+
+        //Overloaded constructor
+        public Course(int courseid, string name, List<Student> Students)
+        {
+            this.CourseId = courseid;
+            this.Name = name;
+            this.Students = Students;
+        }
+
+
+        public string FormatDetailsForDisplay()
+        {
+            return String.Format("Course: 1 - {0} {1}<br/>", this.Name, this.CourseId);
+        }
+    }    
+}
---
+++ CS_projects/ChallengeStudentCourses/ChallengeStudentCourses/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChallengeStudentCourses
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public List<Course> Courses { get; set; }
+
+        public Student()
+        {
+            this.StudentId = 0;
+            this.Name = "Name";
+            this.Courses = Courses;
+        }
+
+        //Overloaded constructor
+        public Student(int studentid, string name, List<Course> Course)
+        {
+            this.StudentId = studentid;
+            this.Name = name;
+            this.Courses = Courses;
+        }
+
+        public string FormatDetailsForDisplay()
+        {
+            return String.Format("Student: 1 - {0} {1}<br/>", this.Name, this.StudentId);
+        }
+    }
+}
