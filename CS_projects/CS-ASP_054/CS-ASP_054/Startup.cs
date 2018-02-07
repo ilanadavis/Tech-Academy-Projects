b00a62bbbe8caf98dacefb571b93062a0975cae5
Ilana Davis <ilana@ilanadavis.com>
2018-02-07 15:05:04 -0800

    practice with switch statement

---
+++ CS_projects/CS-ASP_054/CS-ASP_054/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CS_ASP_054.Startup))]
+namespace CS_ASP_054
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
