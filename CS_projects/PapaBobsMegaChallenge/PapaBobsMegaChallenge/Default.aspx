bfd2a7e0de456c624275ab9c44445650b259df8d
Ilana Davis <ilana@ilanadavis.com>
2018-02-12 14:14:38 -0800

    Add remaining fields using bootstrap & html. Set validation group & causes validation for text boxes.

--- CS_projects/PapaBobsMegaChallenge/PapaBobsMegaChallenge/Default.aspx
+++ CS_projects/PapaBobsMegaChallenge/PapaBobsMegaChallenge/Default.aspx
@@ -26,8 +26,47 @@
                 <asp:ListItem Text="Medium (14 inch - $14)" Value="Medium" />
                 <asp:ListItem Text="Large (16 inch - $16)" Value="Large" />
             </asp:DropDownList>
-                
         </div>
+        <div class="form-group">
+            <label>Crust:</label>
+            <asp:DropDownList ID="crustDropDownList" runat="server" CssClass="form-control">
+                <asp:ListItem Text="Regular" Value="Regular" />
+                <asp:ListItem Text="Thin" Value="Thin" />
+                <asp:ListItem Text="Thick (+ $2)" Value="Thick" />
+            </asp:DropDownList>
+
+        </div>
+        <div class="checkbox"><label><asp:CheckBox ID="sausageCheckBox" runat="server"></asp:CheckBox> Sausage (+ $2)</label></div>
+        <div class="checkbox"><label><asp:CheckBox ID="pepperoniCheckBox" runat="server"></asp:CheckBox> Pepperoni (+ $1.50)</label></div>
+        <div class="checkbox"><label><asp:CheckBox ID="onionsCheckBox" runat="server"></asp:CheckBox> Onions (+ $1)</label></div>
+        <div class="checkbox"><label><asp:CheckBox ID="greenpeppersCheckBox" runat="server"></asp:CheckBox> Green Peppers (+ $1)</label></div>
+
+        <h3>Deliver To:</h3>
+        <div class="form-group">
+            <label>Name:</label>
+            <asp:TextBox ID="nameTextBox" runat="server" CssClass="form-control" CausesValidation="True" ValidationGroup="textbox"></asp:TextBox>
+        </div>
+        <div class="form-group">
+            <label>Address:</label>
+            <asp:TextBox ID="addressTextBox" runat="server" CssClass="form-control" CausesValidation="True" ValidationGroup="textbox"></asp:TextBox>
+        </div>
+        <div class="form-group">
+            <label>Zip:</label>
+            <asp:TextBox ID="zipTextBox" runat="server" CssClass="form-control" CausesValidation="True" ValidationGroup="textbox"></asp:TextBox>
+        </div>
+        <div class="form-group">
+            <label>Phone:</label>
+            <asp:TextBox ID="phoneTextBox" runat="server" CssClass="form-control" CausesValidation="True" ValidationGroup="textbox"></asp:TextBox>
+        </div>
+
+        <h3>Payment:</h3>
+        <div class="radio"><label><asp:RadioButton ID="cashRadioButton" runat="server" Checked="True" GroupName="payment"></asp:RadioButton> Cash</label></div>
+        <div class="radio"><label><asp:RadioButton ID="creditRadioButton" runat="server" GroupName="payment"></asp:RadioButton> Credit</label></div>
+
+        <asp:Button ID="orderButton" runat="server" Text="Order" CssClass="btn btn-primary" />
+
+        <h3>Total Cost:<asp:Label ID="resultLabel" runat="server"></asp:Label></h3>
+
     
     </div>
     </form>
--- CS_projects/PapaBobsMegaChallenge/PapaBobsMegaChallenge/Default.aspx.designer.cs
+++ CS_projects/PapaBobsMegaChallenge/PapaBobsMegaChallenge/Default.aspx.designer.cs
@@ -29,5 +29,122 @@ namespace PapaBobsMegaChallenge {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.DropDownList sizeDropDownList;
+        
+        /// <summary>
+        /// crustDropDownList control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList crustDropDownList;
+        
+        /// <summary>
+        /// sausageCheckBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox sausageCheckBox;
+        
+        /// <summary>
+        /// pepperoniCheckBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox pepperoniCheckBox;
+        
+        /// <summary>
+        /// onionsCheckBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox onionsCheckBox;
+        
+        /// <summary>
+        /// greenpeppersCheckBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox greenpeppersCheckBox;
+        
+        /// <summary>
+        /// nameTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox nameTextBox;
+        
+        /// <summary>
+        /// addressTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox addressTextBox;
+        
+        /// <summary>
+        /// zipTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox zipTextBox;
+        
+        /// <summary>
+        /// phoneTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox phoneTextBox;
+        
+        /// <summary>
+        /// cashRadioButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton cashRadioButton;
+        
+        /// <summary>
+        /// creditRadioButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton creditRadioButton;
+        
+        /// <summary>
+        /// orderButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button orderButton;
+        
+        /// <summary>
+        /// resultLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label resultLabel;
     }
 }
