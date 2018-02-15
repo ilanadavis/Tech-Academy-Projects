e4ad29b8bcbbc6d8b9a1d9ce00d4f9861d36d347
Ilana Davis <ilana@ilanadavis.com>
2018-02-15 14:45:43 -0800

    add enum folder with class files for payment size and crust

---
+++ CS_projects/PapaBobs/PapaBobs.DTO/Enums/PaymentType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PapaBobs.DTO.Enums
+{
+    public enum PaymentType
+    {
+        Cash,
+        Credit
+    }
+}
---
+++ CS_projects/PapaBobs/PapaBobs.DTO/Enums/Size.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PapaBobs.DTO.Enums
+{
+    public enum Size
+    {
+        Small,
+        Medium,
+        Large
+    }
+}
