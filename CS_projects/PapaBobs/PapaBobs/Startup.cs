785c5693e56dd550aefbff0d00464e54f54bebd7
Ilana Davis <ilana@ilanadavis.com>
2018-02-15 13:36:59 -0800

    Build new solution with orders db table. Add projects for domain, persistance and DTO

---
+++ CS_projects/PapaBobs/PapaBobs/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PapaBobs.Startup))]
+namespace PapaBobs
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
