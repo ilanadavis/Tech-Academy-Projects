dea028a26abbc7f9ae0570aa9127d1d3c61357de
Ilana Davis <ilana@ilanadavis.com>
2018-02-15 17:12:07 -0800

    replace pizza class with pizza price manager and build cost determination methods

---
+++ CS_projects/PapaBobs/PapaBobs.Domain/PizzaPriceManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PapaBobs.DTO;
+
+namespace PapaBobs.Domain
+{
+    public class PizzaPriceManager
+    {
+        public static decimal CalculateCost(DTO.OrderDTO order)
+        {
+            decimal cost = 0.0M;
+            var prices = getPizzaPrices();
+            cost += calculateSizeCost(order, prices);
+            cost += calculateCrustCost(order, prices);
+            cost += calculateToppingCost(order, prices);
+            return cost;
+        }
+
+        private static decimal calculateToppingCost(OrderDTO order, DTO.PizzaPriceDTO prices)
+        {
+            decimal cost = 0.0M;
+            cost += (order.Sausage) ? prices.SausageCost : 0M;
+            cost += (order.Pepperoni) ? prices.PepperoniCost : 0M;
+            cost += (order.Onions) ? prices.OnionsCost : 0M;
+            cost += (order.GreenPeppers) ? prices.GreenPeppersCost : 0M;
+            return cost;
+
+        }
+
+        private static decimal calculateCrustCost(OrderDTO order, DTO.PizzaPriceDTO prices)
+        {
+            decimal cost = 0.0M;
+            switch (order.Crust)
+            {
+                case DTO.Enums.CrustType.Regular:
+                    cost = prices.RegularCrustCost;
+                    break;
+                case DTO.Enums.CrustType.Thin:
+                    cost = prices.ThinCrustCost;
+                    break;
+                case DTO.Enums.CrustType.Thick:
+                    cost = prices.ThickCrustCost;
+                    break;
+                default:
+                    break;
+            }
+            return cost;
+        }
+
+        private static decimal calculateSizeCost(OrderDTO order, DTO.PizzaPriceDTO prices)
+        {
+            decimal cost = 0.0M;
+            switch (order.Size)
+            {
+                case DTO.Enums.Size.Small:
+                    cost = prices.SmallSizeCost;
+                    break;
+                case DTO.Enums.Size.Medium:
+                    cost = prices.MediumSizeCost;
+                    break;
+                case DTO.Enums.Size.Large:
+                    cost = prices.LargeSizeCost;
+                    break;
+                default:
+                    break;
+            }
+            return cost;
+        }
+
+        private static DTO.PizzaPriceDTO getPizzaPrices()
+        {
+            var prices = Persistance.PizzaPriceRepository.GetPizzaPrices();
+            return prices;
+        }
+    }
+}
---
+++ CS_projects/PapaBobs/PapaBobs.Persistance/PizzaPriceRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PapaBobs.Persistance
+{
+    public class PizzaPriceRepository
+    {
+        public static DTO.PizzaPriceDTO GetPizzaPrices()
+        {
+            var db = new PapaBobsDbEntities();
+            var prices = db.PizzaPrices.First();
+            var pizzaPriceDTO = convertToDTO(prices);
+            return pizzaPriceDTO;
+
+        }
+
+        private static DTO.PizzaPriceDTO convertToDTO(PizzaPrice pizzaPrice)
+        {
+            var pizzaPriceDTO = new DTO.PizzaPriceDTO();
+
+            pizzaPriceDTO.SmallSizeCost = pizzaPrice.SmallSizeCost;
+            pizzaPriceDTO.MediumSizeCost = pizzaPrice.MediumSizeCost;
+            pizzaPriceDTO.LargeSizeCost = pizzaPrice.LargeSizeCost;
+            pizzaPriceDTO.ThickCrustCost = pizzaPrice.ThickCrustCost;
+            pizzaPriceDTO.ThinCrustCost = pizzaPrice.ThinCrustCost;
+            pizzaPriceDTO.RegularCrustCost = pizzaPrice.RegularCrustCost;
+            pizzaPriceDTO.SausageCost = pizzaPrice.SausageCost;
+            pizzaPriceDTO.PepperoniCost = pizzaPrice.PepperoniCost;
+            pizzaPriceDTO.OnionsCost = pizzaPrice.OnionsCost;
+            pizzaPriceDTO.GreenPeppersCost = pizzaPrice.GreenPeppersCost;
+
+            return pizzaPriceDTO;
+        }
+    }
+}
