76fcf3c92c50a92726be519115fe4bd80f007b9f
Ilana Davis <ilana@ilanadavis.com>
2018-02-15 16:48:34 -0800

    add table for pricing structure

--- CS_projects/PapaBobs/PapaBobs.Persistance/PapaBobsEntities.Context.cs
+++ CS_projects/PapaBobs/PapaBobs.Persistance/PapaBobsEntities.Context.cs
@@ -26,5 +26,6 @@ namespace PapaBobs.Persistance
         }
     
         public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<PizzaPrice> PizzaPrices { get; set; }
     }
 }
