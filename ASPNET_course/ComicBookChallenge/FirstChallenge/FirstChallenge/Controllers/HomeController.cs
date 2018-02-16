b156c1dad2cc11cfffb53df86c6784bd3bd4cf8f
Ilana Davis <ilana@ilanadavis.com>
2018-02-16 12:10:44 -0800

    initial layout for challenge. Add index code to begin display

---
+++ ASPNET_course/ComicBookChallenge/FirstChallenge/FirstChallenge/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var comic = FirstChallenge.Models.ComicBookManager.GetComicBooks();
+            ViewBag.Header = "My Comics";
+            return View();
+        }
+    }
+}
